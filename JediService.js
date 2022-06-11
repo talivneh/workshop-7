00ecd5a01d00063ad0844905838fbf939b154c3a
Tal Livneh <talivneh8@gmail.com>
2022-06-11 10:42:22 +0300

    solution: part 1 finish

--- JediService.js
+++ JediService.js
@@ -1,54 +1,56 @@
-const fs = require('fs').promises;
-const jediFile = 'jedi_list.json';
+const fs = require("fs").promises;
+const jediFile = "jedi_list.json";
 
 async function replaceJedi(jediId, jedi) {
-    //TODO write logic replacing jedi by it's id with newly passed jedi
+  //TODO write logic replacing jedi by it's id with newly passed jedi
 }
 
 async function deleteJedi(id) {
-    //TODO Delete jedi by given id in our file
+  //TODO Delete jedi by given id in our file
 }
 
 async function getAll() {
-    //TODO obtain all saved jedis and return it to callee
+  //TODO obtain all saved jedis and return it to callee
+  const data = await readJediFile();
+  return data;
 }
 
 async function addJedi(jedi) {
-    let data = await readJediFile();
-    if (!data) {
-        data = [];
-    }
-    data.push(jedi);
-    await writeJediFile(data);
+  let data = await readJediFile();
+  if (!data) {
+    data = [];
+  }
+  data.push(jedi);
+  await writeJediFile(data);
 }
 
 async function getJedi(id) {
-    const data = await readJediFile();
-    return data.find((value) => value.id === id);
+  const data = await readJediFile();
+  return data.find((value) => value.id === id);
 }
 
 async function readJediFile() {
-    try {
-        const data = await fs.readFile(jediFile);
-        console.log(data.toString());
-        return JSON.parse(data.toString());
-    } catch (error) {
-        console.error(`Got an error trying to read the file: ${error.message}`);
-    }
+  try {
+    const data = await fs.readFile(jediFile);
+    console.log(data.toString());
+    return JSON.parse(data.toString());
+  } catch (error) {
+    console.error(`Got an error trying to read the file: ${error.message}`);
+  }
 }
 
 async function writeJediFile(content) {
-    try {
-        await fs.writeFile(jediFile, JSON.stringify(content));
-    } catch (error) {
-        console.error(`Failed to write to file ${error.message}`);
-    }
+  try {
+    await fs.writeFile(jediFile, JSON.stringify(content));
+  } catch (error) {
+    console.error(`Failed to write to file ${error.message}`);
+  }
 }
 
 module.exports = {
-    addJedi,
-    getJedi,
-    getAll,
-    replaceJedi,
-    deleteJedi,
+  addJedi,
+  getJedi,
+  getAll,
+  replaceJedi,
+  deleteJedi,
 };
--- app.js
+++ app.js
@@ -1,39 +1,52 @@
-const express = require('express');
-const jediService = require('./JediService');
-const emitted = require('events')
+const express = require("express");
+const jediService = require("./JediService");
+const emitted = require("events");
 const app = express();
 app.use(express.json());
 const port = 8080;
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        health: 'ok'
-    });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    health: "ok",
+  });
 });
 
 app.post("/jedi", async (req, res) => {
-    await jediService.addJedi(req.body);
-    res.status(200).json(req.body);
+  await jediService.addJedi(req.body);
+  res.status(200).json(req.body);
 });
 
 app.get("/jedi/:id", async (req, res) => {
-    let jediId = Number.parseInt(req.params.id);
-    if (isNaN(jediId)) return res.status(400).json({
-        status: 400,
-        error: "wrong parameters"
+  let jediId = Number.parseInt(req.params.id);
+  if (isNaN(jediId))
+    return res.status(400).json({
+      status: 400,
+      error: "wrong parameters",
     });
 
-    const jedi = await jediService.getJedi(jediId);
+  const jedi = await jediService.getJedi(jediId);
 
-    if (!jedi) return res.status(404).json({
-        status: 404,
-        error: "Not found"
+  if (!jedi)
+    return res.status(404).json({
+      status: 404,
+      error: "Not found",
     });
 
-    res.status(200).json(jedi);
+  res.status(200).json(jedi);
 });
 
 //TODO 1. create GET /jedi route and handle logic inside of it
+app.get("/jedi", async (req, res) => {
+  const allJedi = await jediService.getAll();
+
+  if (!allJedi)
+    return res.status(404).json({
+      status: 404,
+      error: "Not found",
+    });
+
+  res.status(200).json(allJedi);
+});
 
 //TODO 2. create PUT /jedi/:id route and handle logic of updating jedi that already exists in the list.
 //Dont forget to take care of errors (e.g. jedi with id not exists)
@@ -41,5 +54,5 @@ app.get("/jedi/:id", async (req, res) => {
 //TODO 3. create DELETE /jedi/:id route and handle logic of deleting jedi
 
 app.listen(port, () => {
-    console.log("Server started on port", port);
+  console.log("Server started on port", port);
 });
